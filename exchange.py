8c98012b905d42c965c94e170603eaa3bc1e1824
SnezhanaM <snezhanamorozova18@gmail.com>
2021-07-28 02:14:26 +0300

    Create file

---
+++ exchange.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+
+data_dict = {}
+
+
+def unified_name(base, quote):
+    return base + '/' + quote
+
+
+r = requests.get('https://aws.okex.com/api/spot/v3/instruments')
+for i in json.loads(r.text):
+    data_dict[unified_name(i['base_currency'], i['quote_currency'])] = ['', i['instrument_id']]
+
+r = requests.get('https://api.binance.com/api/v3/exchangeInfo')
+a = json.loads(r.text)
+
+for i in json.loads(r.text)['symbols']:
+    if unified_name(i['baseAsset'], i['quoteAsset']) in data_dict.keys():
+        data_dict[unified_name(i['baseAsset'], i['quoteAsset'])][0] = i['symbol']
+    else:
+        data_dict[unified_name(i['baseAsset'], i['quoteAsset'])] = [i['symbol'], '']
+
+for k in sorted(data_dict.keys()):
+    print(k, data_dict[k][0], data_dict[k][1], sep=',')
